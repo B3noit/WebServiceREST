26789b7b9ea8fc37fe6bc9a35f692993de014245
Ben <b.lamarre@ludus-academie.com>
2021-06-08 12:53:58 +0200

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 //Connexion à la base
-required_once('connection.php');
+require_once('connection.php');
 
 $sql ='SELECT * FROM `client`';
 
@@ -12,11 +12,53 @@ $query = $db->prepare($sql);
 $query->execute();
 
 //Stockage du résultat dans un tableau associatif
-$result = $query->fetchAll();
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($result);
+require_once('Close.php');
 
 ?>
 
-https://nouvelle-techno.fr/actualites/live-coding-creer-un-crud-en-php
-https://nouvelle-techno.fr/actualites/live-coding-creer-une-api-rest
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Liste des clients</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+	<main class="container">
+		<div class="row">
+			<section class="col-12">
+				<table class="table">
+					<thead>
+						<th>NCLI</th>
+						<th>NOM</th>
+						<th>ADRESSE</th>
+						<th>LOCALITE</th>
+						<th>CAT</th>
+						<th>COMPTE</th>
+					</thead>
+					<tbody>
+						<?php
+						foreach($result as $client)
+						{?>
+							<tr>
+								<td><?= $client['NCLI'] ?></td>
+								<td><?= $client['NOM'] ?></td>
+								<td><?= $client['ADRESSE'] ?></td>
+								<td><?= $client['LOCALITE'] ?></td>
+								<td><?= $client['CAT'] ?></td>
+								<td><?= $client['COMPTE'] ?></td>
+							</tr>
+						<?php
+						}
+						?>
+					</tbody>
+				</table>
+				<a href="add.php" class="btn btn-primary">Ajouter un client</a>
+			</section>
+		</div>
+	</main>
+</body>
+</html>
